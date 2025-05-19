a56880e763faee31577ee38fd9e60154f4a0c29c
AlbertPitram <albertvidallopez02@gmail.com>
2025-05-19 19:21:54 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,18 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ *
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
-        
+       
         twitter: null, // social media usernames
         github: null,
         linkedin: null,
         instagram: null,
+
 
         name: null,
         lastName: null,
@@ -30,20 +31,35 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
+  // Complete Name, role and location
+  let fullName = `${variables.name || "Name"} ${variables.lastName ||
+    "Last Name"}`;
+  let role = variables.role || "Rol";
+  let location = `${variables.city || "City"}, ${variables.country ||
+    "Country"}`;
+
+  // Dynamic social media
+  let socialMediaHTML = "";
+  if (variables.twitter)
+    socialMediaHTML += `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  if (variables.github)
+    socialMediaHTML += `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  if (variables.linkedin)
+    socialMediaHTML += `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  if (variables.instagram)
+    socialMediaHTML += `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+
+  // Final HTML
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+    ${cover}
+    <img src="${variables.avatarURL}" class="photo" />
+    <h1>${fullName}</h1>
+    <h2>${role}</h2>
+    <h3>${location}</h3>
+    <ul class="${variables.socialMediaPosition}">
+      ${socialMediaHTML}
+    </ul>
+  </div>`;
 }
 
 /**
